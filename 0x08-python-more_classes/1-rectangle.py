9c797db9681d4f65fcfba5946e4bc36283f04593
adute <aduti.alemu@gmail.com>
2021-10-06 12:01:04 -0700

    Classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+class Rectangle:
+    __width = 0
+    __height = 0
+
+    def __init(self, width, height):
+        self.__width = width
+        self.__height = height
+    def __width(self):
+        print(self.__width)
+    def __width(self, value):
+        self.__value = __width
+    if isinstance(width, int):
+        try:
+            print()
+        except TypeError:
+            print("width must be an integer")
+    if width < 0:
+        try:
+            print("")
+        except ValueError:
+            print("width must be >= 0")
+
+    def __height(self):
+        print(self.__width)
+    def __height(self, value):
+        self.__value = __height
+        if isinstance(height, int):
+            try:
+                print()
+            except TypeError:
+                print("height must be an integer")
+        if height < 0:
+            try:
+                print()
+            except ValueError:
+                print("height must be >= 0")
+
