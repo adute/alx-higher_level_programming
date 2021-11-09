9cce7c9d33505eae33e4ec847929ac613fa7afd6
adute <aduti.alemu@gmail.com>
2021-11-09 11:07:07 -0800

    editted

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 99):
     print("{} = 0x{}".format(i, str(hex(i))[2:]))
-
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def uppercase(str):
     strNew = ''
     for i in range(len(str)):
